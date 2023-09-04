c910b06a52df73e8babf4789c94bdd53e3362abb
seema-chaudhary <seemaee.ch@gmail.com>
2023-09-04 18:08:48 +0530

    Addes @PathVariable Example

--- src/main/java/com/springmvc/HomeController.java
+++ src/main/java/com/springmvc/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -68,5 +69,23 @@ public class HomeController {
 	{		
 		String url="https://www.google.com/search?q="+keyword;
 		return "redirect:"+url;
+	}
+	
+	
+////	@PathVariable Example for single Value
+//	@RequestMapping("/user/{id}")
+//	public String search(@PathVariable("id")int id)
+//	{		
+//		System.out.println("Id = "+ id);
+//		return "home";
+//	}
+	
+//	@PathVariable Example for double Value
+	@RequestMapping("/user/{id}/{name}")
+	public String search(@PathVariable("id")int id, @PathVariable String name)
+	{		
+		System.out.println("Id = "+ id);
+		System.out.println("Name = "+ name);
+		return "home";
 	}
 }
